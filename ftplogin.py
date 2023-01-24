9f5a2fbd8c78ab30a45e9f16cf2e9fd8279aaf2c
Rajesh1308 <rajesharulrathna@gmail.com>
2023-01-24 20:09:46 +0530

    Comments added

--- ftplogin.py
+++ ftplogin.py
@@ -1,8 +1,14 @@
+#importing requirements
+
 import ftplib
 import socket
 
+#anonlogin checks for anonymous login in the host provided
+
 def anonlogin(hostname):
     try:
+        #connecting with FTP
+
         ftp = ftplib.FTP(hostname)
         ftp.login('anonymous')
         print("[+] FTP Anonymous login successful \n")
@@ -12,5 +18,7 @@ def anonlogin(hostname):
 
 if __name__ == '__main__':
     hostname = input(">>Enter the host name : ")
+    #Converting hostname to its IP address
+
     hostname = socket.gethostbyname(hostname)
     anonlogin(hostname)
